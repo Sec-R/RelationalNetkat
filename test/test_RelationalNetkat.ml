31e84dc248d5883e999fdb4a1da1606b5fb995da
Sec R <33368868+Sec-R@users.noreply.github.com>
2024-12-15 15:13:48 -0500

    Reachable Test

--- test/test_RelationalNetkat.ml
+++ test/test_RelationalNetkat.ml
@@ -103,6 +103,9 @@ let tests = "MLBDD tests" >::: [
         let bdd22 = RN.compile_pkr_bdd man pk1 pk2 (RN.Comp ((RN.RightAsgn (3, true)),(RN.Or (RN.LeftAsgn (1, true),RN.LeftTest (2, true))))) in
         let bdd23 = RN.compile_pkr_bdd man pk1 pk2 (RN.Or (RN.Comp (RN.RightAsgn (3, true),RN.LeftAsgn (1, true)),RN.Comp(RN.RightAsgn (3, true),RN.LeftTest (2, true)))) in
         assert_equal ~cmp:MLBDD.equal bdd22 bdd23;
+        let bdd24 = RN.compile_pkr_bdd man pk1 pk2 (RN.Comp (RN.RightTest (1, true),RN.RightTest (2, true))) in
+        let bdd25 = (MLBDD.dand (RN.compile_pkr_bdd man pk1 pk2 (RN.RightAsgn (1, true))) (RN.compile_pkr_bdd man pk1 pk2 (RN.RightAsgn (2, true)))) in
+        assert_equal ~cmp:MLBDD.equal bdd24 bdd25;
       );
       "compile_delta_k_test" >:: (fun _ctx ->
         let man = RN.init_man 10 10 in
@@ -243,9 +246,27 @@ let tests = "MLBDD tests" >::: [
           let pk2 = 1 in
           let pk3 = 2 in
           let pk4 = 3 in
-          let compare = RN.NKROMap.equal MLBDD.equal in
           let nkromap1 = RN.calculate_reachable_set man pk1 pk2 pk3 pk4 (RN.NK.Star RN.NK.Dup,RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (RightAsgn (1,true))) (RN.SR.add (RN.Rel.Right (RightAsgn (2,true))) RN.SR.empty)))) in
-          print_endline (RN.nkro_map_to_string nkromap1);
+          let bdd1 = RN.NKROMap.find (Some (RN.NK.Star RN.NK.Dup),Some (RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (RightAsgn (1,true))) (RN.SR.add (RN.Rel.Right (RightAsgn (2,true))) RN.SR.empty))))) nkromap1 in
+          let bdd2 = RN.produce_id man pk1 pk3 in
+          assert_equal ~cmp:MLBDD.equal bdd1 bdd2;
+          let nkromap2 = RN.calculate_reachable_set man pk1 pk2 pk3 pk4 ( (RN.NK.Star (RN.NK.Asgn (1,true))),  (RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (RightAsgn (1,true))) (RN.SR.add (RN.Rel.Right (RightAsgn (2,true))) RN.SR.empty))))) in
+          let bdd3 = (MLBDD.dand bdd2 (MLBDD.dand (RN.compile_pkr_bdd man pk3 pk4 (RN.RightAsgn (2, true))) (RN.compile_pkr_bdd man pk1 pk2 (RN.RightAsgn (1, true))))) in
+          let support13 = RN.generate_double_support man pk1 pk3 in
+          let bdd4 = (RN.rename_bdd pk2 pk1 (RN.rename_bdd pk4 pk3 (MLBDD.exists support13 bdd3))) in
+          let bdd5 = RN.NKROMap.find (None,None) nkromap2 in
+          assert_equal ~cmp:MLBDD.equal bdd4 bdd5;
+          let bdd6 = RN.NKROMap.find (None,Some (RN.Rel.SeqR (RN.Rel.Right (RightAsgn (2,true)), (RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (RightAsgn (1,true))) (RN.SR.add (RN.Rel.Right (RightAsgn (2,true))) RN.SR.empty))))))) nkromap2 in
+          let bdd7 =  (MLBDD.dand bdd2  (MLBDD.dand (RN.compile_pkr_bdd man pk3 pk4 (RN.RightAsgn (2, true))) (RN.compile_pkr_bdd man pk1 pk2 (RN.RightAsgn (1, true))))) in
+          let bdd8 = (RN.rename_bdd pk2 pk1 (RN.rename_bdd pk4 pk3 (MLBDD.exists support13 bdd7))) in
+          assert_equal ~cmp:MLBDD.equal bdd6 bdd8;
+          let nkromap3 = RN.calculate_reachable_set man pk1 pk2 pk3 pk4 ( (RN.NK.Star (RN.NK.Asgn (1,true))),  (RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (RightAsgn (2,true))) (RN.SR.add (RN.Rel.Right (RightAsgn (3,true))) RN.SR.empty))))) in
+          let bdd9 = MLBDD.dand bdd2 (MLBDD.dand (RN.compile_pkr_bdd man pk3 pk4 (RN.RightAsgn (3, true))) (RN.compile_pkr_bdd man pk1 pk2 (RN.RightTest (2, true)))) in
+          let bdd10 = (RN.rename_bdd pk2 pk1 (RN.rename_bdd pk4 pk3 (MLBDD.exists support13 bdd9))) in
+          let bdd11 = RN.NKROMap.find (None,None) nkromap3 in
+          assert_equal ~cmp:MLBDD.equal bdd10 bdd11;
+          let nkromap4 = RN.calculate_reachable_set man pk1 pk2 pk3 pk4 ((RN.NK.Seq (RN.NK.Asgn (1,true),(RN.NK.Star (RN.NK.Asgn (1,true))))),  (RN.Rel.StarR (RN.Rel.OrR (RN.SR.add (RN.Rel.Left (LeftTest (1,false))) (RN.SR.add (RN.Rel.Right (RightAsgn (3,true))) RN.SR.empty))))) in
+          assert_equal (Option.is_none (RN.NKROMap.find_opt (None,None) nkromap4)) true;
         );
   
       ]
